abd676757d16dc662a44879d2f018a8004f800f4
Jack Storrs <jackstorrs8@gmail.com>
2016-11-11 13:48:37 -0500

    Create a route for logging in, and define method 'login' which verifies the username and password and generates access based on a boolean check.

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -12,6 +12,25 @@ class UserController {
 
       response.json(user)
   }
+
+  * login(request, response) {
+      let data = request.only('username', 'password')
+      let user = yield User.findBy('username', data.username)
+      try {
+        if(user) {
+          let verify = yield Hash.verify(data.password, 'password')
+        }
+          if(verify) {
+            let token = yield request.auth.generate(user)
+            user.token = token
+          } else { throw new Error(); }
+
+          }
+
+      } catch(e) {
+        response.status(401).json({ error: "Wrong username or password" })
+      }
+  }
 }
 
 module.exports = UserController
