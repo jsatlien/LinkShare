a5ed2d9718e9537bd9bf1209ab791e1363f7f14f
Jack Storrs <jackstorrs8@gmail.com>
2016-11-11 13:20:19 -0500

    added register method to the UserController, modified the user model to hide sensitive information in the reponse.json.

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -1,7 +1,17 @@
 'use strict'
 
+const Hash = use('Hash')
+const User = use('App/Model/User')
+
 class UserController {
 
+  * register(request, response) {
+      let data = request.only('email', 'username', 'password')
+      data.password = yield Hash.make(data.password)
+      let user = yield User.create(data)
+
+      response.json(user)
+  }
 }
 
 module.exports = UserController
