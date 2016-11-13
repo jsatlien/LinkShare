96eb1e44d829589d5b98ab7c4f24aa826ffc891a
Jack Storrs <jackstorrs8@gmail.com>
2016-11-13 18:52:49 -0500

    Fixed a syntax error in my 'show all posts' route and added the method 'show' to PostController.

--- app/Http/Controllers/PostController.js
+++ app/Http/Controllers/PostController.js
@@ -12,6 +12,16 @@ class PostController {
 
     response.json(newPost)
   }
+  * show (request, response) {
+    let userId = request.param('id')
+    let posts = yield Post.query().where('user_id', userId)
+    if (posts) {
+      response.json(posts)
+    } else {
+      response.status(401).json({ error: 'This user currently has no posts.'})
+     }
+  }
 }
+
 
 module.exports = PostController
