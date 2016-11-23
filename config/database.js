fb401bc867b9918329bc9a013dfcb73075eeae3d
Jack Storrs <jackstorrs8@gmail.com>
2016-11-23 15:27:18 -0500

    Added postgres and updated package.json and database files.

--- config/database.js
+++ config/database.js
@@ -66,13 +66,8 @@ module.exports = {
   |
   */
   pg: {
-    client: 'pg',
-    connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    }
-  }
+  client: 'pg',
+  connection: Env.get('DATABASE_URL')
+}
 
 }
