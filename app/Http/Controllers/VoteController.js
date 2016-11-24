db73edc3f45fd2273fb58f0246ead6a1e47e8680
Jack Storrs <jackstorrs8@gmail.com>
2016-11-23 23:08:12 -0500

    500 Error so merging this branch and pushing to heroku.

--- app/Http/Controllers/VoteController.js
+++ app/Http/Controllers/VoteController.js
@@ -7,18 +7,16 @@ class VoteController {
   * create (request, response) {
     let currentUser = request.authUser
     let postId = request.param('post_id')
-    let postUser = request.param('post_id')
     let data = {}
-    data.user_id = user.id
+    data.user_id = Currentuser.id
     data.post_id = postId
 
     let voteCheck = yield Vote.query()
       .where({'post_id': postId,
-              'user_id': user.id }).select('vote_count')
-    console.log(voteCheck)
-    if (voteCheck !== 1 ) {
-      let addVote =yield Vote.create(data)
+              'user_id': currentUser.id });
 
+    if (!voteCheck) {
+      let addVote = yield Vote.create(data)
       let post = yield Post.findBy('id', data.post_id)
       if (post.vote_count = null ) {
         post.vote_count = 1
@@ -27,7 +25,9 @@ class VoteController {
       }
       yield post.save()
 
-      response.json([addVote, post])
+      response.status(202).json({'New Vote' : addVote,
+                         'Post' : post
+                       });
     } else {
       response.status(401).json({error: "User can only cast 1 vote per post."})
     }
