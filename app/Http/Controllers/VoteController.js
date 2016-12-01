59dcaaacf70ad97428abd8d3e7080aa3b980cc08
Jack Storrs <jackstorrs8@gmail.com>
2016-12-01 10:27:16 -0500

    Change conditional on voteCheck to an array length check.

--- app/Http/Controllers/VoteController.js
+++ app/Http/Controllers/VoteController.js
@@ -14,24 +14,24 @@ class VoteController {
     let voteCheck = yield Vote.query()
       .where({'post_id': postId,
               'user_id': currentUser.id })
-    console.log(voteCheck)
+    console.log('voteCheck', voteCheck)
 
-    if (voteCheck) {
+    if (voteCheck.length) {
       response.status(401).json({error: "User can only cast 1 vote per post."})
     } else {
-      let addVote = yield Vote.create(data)
-      yield addVote.save();
-      let post = yield Post.findBy('id', data.post_id)
-      let votes = post.vote_count;
-      if (votes === null ) {
-        votes = 1;
-      } else {
-        votes++;
-      }
-      post.vote_count = votes;
-      yield post.save()
+        let addVote = yield Vote.create(data)
+        yield addVote.save();
+        let post = yield Post.findBy('id', data.post_id)
+        let votes = post.vote_count;
+        if (votes === null ) {
+          votes = 1;
+        } else {
+          votes++;
+        }
+        post.vote_count = votes;
+        yield post.save()
 
-      response.status(202).json([post, addVote]);
+        response.status(202).json([post, addVote]);
     }
 
   }
